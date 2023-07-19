a8daebdbd6cb01535aeb1c4cb8a5d8de565e2de8
qinxinghua <xinghua.qin@fenbeitong.com>
2023-07-19 09:03:53 +0800

    创建项目

---
+++ myEasyAlgorithmsDemo/myEasyAlgorithmsDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  myEasyAlgorithmsDemo
+//
+//  Created by qinxinghua on 2023/7/19.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
